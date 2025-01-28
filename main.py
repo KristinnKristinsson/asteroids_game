bc347fa4b06eeaf66e3a3ac4dbc332ba40d51295
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-01-28 13:20:29 +0100

    Groups created.

--- main.py
+++ main.py
@@ -8,15 +8,26 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     triangle_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     fps = pygame.time.Clock()
     dt = 0
+
+    
+    #screen.containers = (drawable)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0)
-        triangle_player.draw(screen)
+        
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         fps.tick(60)
         dt = (fps.tick(60)/1000)
