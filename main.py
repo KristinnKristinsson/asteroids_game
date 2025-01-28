61eb0a6c1ee11ed977757a4a6f73e36ef5a7e5b4
KristinnKristinsson <kristinn.agust.kristinsson@gmail.com>
2025-01-28 15:38:25 +0100

    Got some asteroidfield going on

--- main.py
+++ main.py
@@ -4,13 +4,19 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    ast_field = AsteroidField()
     triangle_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     fps = pygame.time.Clock()
     dt = 0
